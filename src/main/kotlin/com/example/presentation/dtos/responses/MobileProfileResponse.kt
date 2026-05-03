d8ef8c03e9a4030956cb521189aad09d9725d845
MEJRI Jihene <jihene.mejri@bforbank.com>
2026-05-03 21:27:03 +0200

    JM : Fix  clean archi presentation layer don't handle dto but mobile response

--- src/main/kotlin/com/example/presentation/dtos/responses/MobileProfileResponse.kt
+++ src/main/kotlin/com/example/presentation/dtos/responses/MobileProfileResponse.kt
@@ -1,10 +1,10 @@
 package com.example.presentation.dtos.responses
 
-import com.example.presentation.dtos.MobileCardDTO
+import com.example.presentation.responses.MobileCardResponse
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MobileProfileResponse(
     val userName: String,
-    val cards: List<MobileCardDTO>
+    val cards: List<MobileCardResponse>
 )
